1c737ccf32d08d8d66c1cd4ed7ff6ae4a994d6fa
Yahya Ergun <yahya.ergun@bestseller.com>
2019-02-17 12:26:59 +0100

    q

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'favorite';
+  title = 'this is a long text which has one of the most distributed title case ever.';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FavstarComponent } from './favstar/favstar.component';
+import { TitleCasePipe } from './titlecase.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavstarComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule
