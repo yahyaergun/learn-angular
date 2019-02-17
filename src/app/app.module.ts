6106c6d6ffef894b4e0316dd5f5b7eebae95f1b6
Yahya Ergun <yahya.ergun@bestseller.com>
2019-02-17 20:57:22 +0100

    zippy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FavstarComponent } from './favstar/favstar.component';
 import { TitleCasePipe } from './titlecase.pipe';
 import { LikeComponent } from './like/like.component';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FavstarComponent,
     TitleCasePipe,
-    LikeComponent
+    LikeComponent,
+    ZippyComponent
   ],
   imports: [
     BrowserModule
